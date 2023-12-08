9593b848cde7bf0021f76069809c34d59df19d8b
l4p1n <l4p1n@l4p1n.ch>
2023-12-08 14:44:41 +0100

    Resolve day 8 part 2 with LCM

--- Day8/Program.cs
+++ Day8/Program.cs
@@ -9,7 +9,7 @@ class Program
 
     public static void Main(string[] args)
     {
-        var filename = args.Length > 0 ? args[0] : "files/p2_test.txt";
+        var filename = args.Length > 0 ? args[0] : "files/input.txt";
         var file_content = File.ReadAllLines(filename);
 
         var left_right_tape = file_content[0].ToCharArray();
@@ -25,47 +25,50 @@ class Program
         .ToDictionary(node => node.NodeName);
 
         // Part 1
-        var tape_step = 0;
-
         {
+            var tape_step = 0;
             var current_node = nodes["AAA"];
             do
             {
                 var current_instruction = left_right_tape[tape_step % left_right_tape.Length];
                 var next_node_name = current_instruction == 'L' ? current_node.LeftNode : current_node.RightNode;
-                Console.WriteLine($"{tape_step} {current_node.NodeName} {current_instruction} => {next_node_name}");
+                // Console.WriteLine($"{tape_step} {current_node.NodeName} {current_instruction} => {next_node_name}");
                 current_node = nodes[next_node_name];
                 tape_step += 1;
             } while (current_node.NodeName != "ZZZ");
+            Console.WriteLine($"Part 1 of steps required: {tape_step} steps");
         }
-
-        Console.WriteLine($"Part 1 of steps required: {tape_step} steps");
 
         // Part 2
-        /*tape_step = 0;
-        {
-            var current_nodes = nodes.Where(pair => pair.Key.EndsWith("A")).Select(pair => pair.Value).ToArray();
-            // Sanity check the puzzle input before going into the loop
-            if (nodes.Where(pair => pair.Key.EndsWith("Z")).Count() != current_nodes.Length)
-                throw new Exception("Sanity check: Count of nodes ending in A is not equal to the count of nodes ending in Z");
+        var starting_nodes = nodes.Where(pair => pair.Key.EndsWith("A")).Select(pair => pair.Value).ToArray();
+        // Sanity check the puzzle input before going into the loop
+        if (nodes.Where(pair => pair.Key.EndsWith("Z")).Count() != starting_nodes.Length)
+            throw new Exception("Sanity check: Count of nodes ending in A is not equal to the count of nodes ending in Z");
 
+        var part2_required_steps = starting_nodes.Select(starting_node =>
+        {
+            var current_node = starting_node;
+            ulong steps = 0;
             do
             {
-                var current_instruction = left_right_tape[tape_step % left_right_tape.Length];
-                for(int i = 0; i < current_nodes.Length; i++) {
-                    var current_node = current_nodes[i];
-                    var next_node_name = current_instruction == 'L' ? current_node.LeftNode : current_node.RightNode;
-                    current_nodes[i] = nodes[next_node_name];
-                    //Console.WriteLine($"{tape_step} N{i + 1} {current_node.NodeName} {current_instruction} => {next_node_name}");
-                }
-                tape_step += 1;
+                var current_instruction = left_right_tape[steps % (ulong)left_right_tape.Length];
+                var next_node_name = current_instruction == 'L' ? current_node.LeftNode : current_node.RightNode;
+                current_node = nodes[next_node_name];
+                steps += 1;
+            } while (!current_node.NodeName.EndsWith("Z"));
+            Console.WriteLine($"Node: {starting_node.NodeName} = {steps}");
+            return steps;
+        }).Aggregate(1uL, LCM);
+        Console.WriteLine($"Part 2: required steps = {part2_required_steps}");
+    }
 
-                if(tape_step % 500000 == 0){
-                    Console.WriteLine($"Pending: current step = {tape_step}");
-                }
-            } while (current_nodes.Where(node => node.NodeName.EndsWith("Z")).Count() != current_nodes.Length);
+    private static ulong LCM(ulong n1, ulong n2) => n1 * n2 / GCD(n1, n2);
+
+    private static ulong GCD(ulong n1, ulong n2) {
+        if(n2 == 0){
+            return n1;
         }
 
-        Console.WriteLine($"Part 2 of steps required: {tape_step} steps");*/
+        return GCD(n2, n1 % n2);
     }
 }
