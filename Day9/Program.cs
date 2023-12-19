f0e8f6e426aca0dbd5460c86506920834977a4fb
l4p1n <l4p1n@l4p1n.ch>
2023-12-19 13:23:07 +0100

    Add day 9 part 2
    Bonus: variables renaming for clarity (I hope)

--- Day9/Program.cs
+++ Day9/Program.cs
@@ -1,4 +1,6 @@
 
+record ExtrapolatedValues(int Forward, int Backward);
+
 class Program
 {
     public static void Main(string[] args)
@@ -7,23 +9,25 @@ class Program
         var file_content = File.ReadAllLines(filename);
 
         var puzzle_input = file_content.Select(line => line.Split(' ').Select(i => Convert.ToInt32(i)).ToArray()).ToArray();
-        var predictions = puzzle_input.Select(i => OASIS_Prediction(i)).ToArray();
-        Console.WriteLine($"Part 1: extrapolated values {string.Join(' ', predictions)}");
-        var predictions_sum = predictions.Sum();
-        Console.WriteLine($"Part 1: sum {predictions_sum}");
+        var extrapolated_values = puzzle_input.Select(i => OASIS_Extrapolation(i)).ToArray();
+        var extrapolated_forward_sum = extrapolated_values.Sum(v => v.Forward);
+        var extrapolated_backward_sum = extrapolated_values.Sum(v => v.Backward);
+
+        Console.WriteLine($"Forward extrapolated values sum (part 1): {extrapolated_forward_sum}");
+        Console.WriteLine($"Backward extrapolated values sum (part 2): {extrapolated_backward_sum}");
     }
 
-    private static int OASIS_Prediction(int[] input) {
-        return OASIS_Prediction(input, 1);
+    private static ExtrapolatedValues OASIS_Extrapolation(int[] input) {
+        return OASIS_Extrapolation(input, 1);
     }
 
-    private static int OASIS_Prediction(int[] input, int step)
+    private static ExtrapolatedValues OASIS_Extrapolation(int[] input, int step)
     {
         Console.WriteLine($"Current step {step}: {string.Join(' ', input)}");
         // End of recursion clause: if all the input items are zero, we halt here by returning a zero
         if(input.All(item => item == 0)){
-            Console.WriteLine($"Extrapolated step {step}: 0");
-            return 0;
+            Console.WriteLine($"Extrapolated step {step}: 0 0");
+            return new ExtrapolatedValues(0, 0);
         }
 
         var initial_sequence = input.Zip(input[1..], (element, next_element) => new int[]{ element, next_element });
@@ -33,10 +37,12 @@ class Program
             next_input.Add(item[1] - item[0]);
         }
 
-        var extrapolated_value_step_below = OASIS_Prediction(next_input.ToArray(), step + 1);
-        var extrapolated_value_current_step = extrapolated_value_step_below + input[^1];
-        Console.WriteLine($"Extrapolated step {step}: {extrapolated_value_current_step}");
+        var extrapolated_values_step_below = OASIS_Extrapolation(next_input.ToArray(), step + 1);
+        var extrapolated_forward = extrapolated_values_step_below.Forward + input[^1];
+        var extrapolated_backward = input[0] - extrapolated_values_step_below.Backward;
+        var extrapolated_values = new ExtrapolatedValues(extrapolated_forward, extrapolated_backward);
+        Console.WriteLine($"Extrapolated step {step}: {extrapolated_values}");
 
-        return extrapolated_value_current_step;
+        return extrapolated_values;
     }
 }
